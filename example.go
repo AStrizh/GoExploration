01f477a18a00acf120f6ef2600218104ec473218
Aleksandr Strizhevskiy <abstrizhevskiy@gmail.com>
2024-05-01 05:08:09 +0000

    Learned declarations and function calls

--- example.go
+++ example.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
 	fmt.Print("Hello World\n")
+
+	fmt.Println(investmentCalculator(1000,5.5,10))
+
+	y := 7
+	x := 5
+
+
+	fmt.Println(x+y)
+
+
+}
+
+func investmentCalculator(amount float64, rate float64, years int) float64{
+	var futureValue = amount * math.Pow(1+rate/100,float64(years))
+	return futureValue
 }
